1199632d264b39a2dfc9ea14bc44c60dee69682d
Chris McKenzie <kristopolous@yahoo.com>
2014-09-18 21:06:34 -0700

    using titles

--- addoffender.php
+++ addoffender.php
@@ -1,6 +1,35 @@
 <?php
 include('db.php');
 
+function get_title($url) {
+  static $uaList = Array(
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.41 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:25.0) Gecko/20100101 Firefox/25.0'
+  );
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 4);
+  curl_setopt($ch, CURLOPT_USERAGENT, $uaList[array_rand($uaList)]);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+  $data = curl_exec($ch);
+  curl_close($ch);
+  // parse the html
+  $doc = new DOMDocument();
+  @$doc->loadHTML($data);
+  $name = '';
+  // use the title as the name
+  $titleList = $doc->getElementsByTagName("title");
+  foreach($titleList as $title) {
+    $name = $title->nodeValue;
+  }
+  return trim($name);
+}
+
 $raw_url = $_POST['url'];
 $url = $db->escapeString($raw_url);
 $res = $db->query('select * from sites where url="' . $url . '"');
@@ -9,14 +38,25 @@ $row = $res->fetchArray();
 if(!$row) {
   $md5 = md5($url);
   $site = escapeshellarg($raw_url);
-  exec('DISPLAY=:10 cutycapt --min-height=768 --min-width=1024 --url=' . $site . ' --out=img/' . $md5 . '.png');
-  // only continue if the image was successfully made.
+  if($_SERVER['HTTP_HOST'] == 'localhost') { 
+    $display = ":0";
+  } else {
+    $display = ":10";
+  }
+  exec('DISPLAY='.$display.' cutycapt --min-height=768 --min-width=1024 --url=' . $site . ' --out=img/' . $md5 . '.png');
 
+  // only continue if the image was successfully made.
   if(file_exists('img/' . $md5 . '.png')) {
     exec('convert img/' . $md5 . '.png -resize 300x -crop 300x320+0+0 -resize 300x img/' . $md5 . '_tn.jpg');
+    $title = $db->escapeString(get_title($url));
     $db->exec('
-      insert into sites (url, up, down, view) 
-      values("' . $url . '", 1, 0, 1)');
+      insert into sites (url, title, up, down, view) 
+      values("' . $url . '", "' . $title . '", 1, 0, 1)');
+
+    if($db->lastErrorCode() !== 0) {
+      echo $db->lastErrorMsg();
+      exit(0);
+    }
   } else {
     echo "Oh shit ... couldn't get screen shot.";
     die;
--- index.php
+++ index.php
@@ -12,7 +12,7 @@ function clean($str) {
 <link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
 <style>
 body{background:url('subtle_grunge.png');text-align:center;margin:0;padding:0}
-#header{font-family: 'Poiret One', cursive;color:#ccc;font-weight:normal;background:rgba(191,191,191,0.2);padding:1em}
+#header{font-family: 'Poiret One', cursive;color:#aaa;font-weight:normal;background:rgba(191,191,191,0.1);padding:1em}
 #header p {color:#aaa;font-size:1.15em}
 h1{font-weight:normal;font-size:4em;padding:0;margin:0}
 li {
@@ -26,6 +26,7 @@ li {
 }
 input{padding: 0.25em}
 input[type='text']{border: 2px solid rgba(80,80,80,0.5)}
+input[type="submit"]{padding: inherit 2em;}
 form { margin-bottom: 1em}
 form > * { font-size: 130% }
 li img { border: 2px solid rgba(0,0,0,0.25); padding: 0.15em; margin-bottom: 1em }
@@ -34,29 +35,33 @@ li img { border: 2px solid rgba(0,0,0,0.25); padding: 0.15em; margin-bottom: 1em
   padding: 0.5em;
   margin-bottom: 1.5em;
 }
+a, a:visited { color: darkblue; text-decoration: none }
+a:hover { text-decoration: underline;color:blue }
+a,#copy{
+  font-family: 'Droid Sans', sans-serif;
+}
 #copy {
   font-size: 1.2em;
-  font-family: 'Droid Sans', sans-serif;
   display: inline-block;
   width: 30em;
   text-align: left;
   line-height: 1.5em;
 }
 ul { text-align: left }
-#joke { border-radius: 12px }
+#joke { vertical-align: top; border-radius: 12px }
 p { margin: 0.5em}
 </style><div id=header>
 <h1>Low Contrast Offenders</h1>
 <p>who the hell can read this shit</p>
 </div>
 <div id='content'>
-<img id='joke' src='hipster.jpg'>
-<div id="copy">
-<p>Sometimes people have the cool-kid designer stick shoved sooo far up their ass that they forgot to make their content readable.</p>
-<p>Fuck them.</p>
-<p>This is where we expose shitty low-contrast impossible to read websites. </p>
-<p>Screw those people. Really.</p>
-</div>
+  <img id='joke' src='hipster.jpg'>
+  <div id="copy">
+    <p>Some people have the cool-kid designer stick shoved so far up their ass that they forgot to make their content readable.</p>
+    <p>Fuck them. This has gone on too long.</p>
+    <p>Let's expose low-contrast impossible to read websites.</p>
+    <p>Screw those people. Really.</p>
+  </div>
 </div>
 
 <form method='post' action="addoffender.php">
@@ -73,7 +78,7 @@ p { margin: 0.5em}
   while( $row = $res->fetchArray() ) {
     echo "<li>";
     echo "<a target=_blank href=" . $row['url'] . "><img src=img/" . md5($row['url']) . "_tn.jpg></a><br>";
-    echo "<a target=_blank href=" . $row['url'] . ">" . clean($row['url']) . "</a>";
+    echo "<a target=_blank href=" . $row['url'] . ">" . clean($row['title']) . "</a>";
     echo "</li>";
   }
 ?>
