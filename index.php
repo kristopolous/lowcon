1e03f4079be8b58630e453d6a181c01a3ae65170
Chris McKenzie <kristopolous@yahoo.com>
2014-09-18 18:58:26 -0700

    a bit of style

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <style>
+body{background:url('subtle_grunge.png')}
+form,p,h1 { text-align: center } 
 li {
+  background: rgba(255,255,255,0.7);
   list-style: none;
   text-align: center;
+  padding: 1.25em;
+  box-shadow: 0 0 5px 2px rgba(127,127,127,0.5);
   display: inline-block;
 }
-li img { border: 2px solid rgba(0,0,0,0.5); margin-bottom: 0.5em }
+form { margin-bottom: 3em}
+form > * { font-size: 130% }
+li img { border: 2px solid rgba(0,0,0,0.25); padding: 0.15em; margin-bottom: 1em }
 </style>
 <h1>Low Contrast Offenders</h1>
 <p>This is where we expose shitty low-contrast impossible to read websites.</p>
-<p>Expose this morons.</p>
 
 <form method='post' action="addoffender.php">
-  <input name="url">
+  <input size=30 name="url">
   <input type="submit" value="Add Offender">
 </form>
 
