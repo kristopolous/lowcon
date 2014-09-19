495730492f79a523a90f231756cb263ca0491208
Chris McKenzie <kristopolous@yahoo.com>
2014-09-18 19:10:12 -0700

    updating spacing

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+function clean($str) {
+  if(strlen($str) > 35) {
+    return substr($str, 0, 35) . '&hellip;';
+  } 
+  return $str;
+}
+?>
 <style>
 body{background:url('subtle_grunge.png')}
 form,p,h1 { text-align: center } 
@@ -6,9 +14,11 @@ li {
   list-style: none;
   text-align: center;
   padding: 1.25em;
-  box-shadow: 0 0 5px 2px rgba(127,127,127,0.5);
+  margin: 0 1em;
+  box-shadow: 0 0 3px 2px rgba(127,127,127,0.5);
   display: inline-block;
 }
+input{padding: 0.25em}
 form { margin-bottom: 3em}
 form > * { font-size: 130% }
 li img { border: 2px solid rgba(0,0,0,0.25); padding: 0.15em; margin-bottom: 1em }
@@ -17,18 +27,19 @@ li img { border: 2px solid rgba(0,0,0,0.25); padding: 0.15em; margin-bottom: 1em
 <p>This is where we expose shitty low-contrast impossible to read websites.</p>
 
 <form method='post' action="addoffender.php">
-  <input size=30 name="url">
+  <input size=40 name="url">
   <input type="submit" value="Add Offender">
 </form>
 
 <ul>
 <?php
+
   include('db.php');
   $res = $db->query("select * from sites order by up limit 20");
 
   while( $row = $res->fetchArray() ) {
     echo "<li><img src=img/" . md5($row['url']) . "_tn.jpg><br>";
-    echo "<a href=" . $row['url'] . ">" . $row['url'] . "</a>";
+    echo "<a href=" . $row['url'] . ">" . clean($row['url']) . "</a>";
     echo "</li>";
   }
 ?>
