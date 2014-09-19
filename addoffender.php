0d1bb361018b07cfbc0ad9f64d03bdbfc287b928
Chris McKenzie <kristopolous@yahoo.com>
2014-09-18 20:28:58 -0700

    fixing for bad screen shot

--- addoffender.php
+++ addoffender.php
@@ -10,10 +10,17 @@ if(!$row) {
   $md5 = md5($url);
   $site = escapeshellarg($raw_url);
   exec('DISPLAY=:10 cutycapt --min-height=768 --min-width=1024 --url=' . $site . ' --out=img/' . $md5 . '.png');
-  exec('convert img/' . $md5 . '.png -resize 300x -crop 300x320+0+0 -resize 300x img/' . $md5 . '_tn.jpg');
-  $db->exec('
-    insert into sites (url, up, down, view) 
-    values("' . $url . '", 1, 0, 1)');
+  // only continue if the image was successfully made.
+
+  if(file_exists('img/' . $md5 . '.png')) {
+    exec('convert img/' . $md5 . '.png -resize 300x -crop 300x320+0+0 -resize 300x img/' . $md5 . '_tn.jpg');
+    $db->exec('
+      insert into sites (url, up, down, view) 
+      values("' . $url . '", 1, 0, 1)');
+  } else {
+    echo "Oh shit ... couldn't get screen shot.";
+    die;
+  }
 }
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 
