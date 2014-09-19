b4d2d3a9218e98bbad7c9e5302bf71bad6071829
Chris McKenzie <kristopolous@yahoo.com>
2014-09-18 19:35:23 -0700

    more joke

--- index.php
+++ index.php
@@ -6,9 +6,11 @@ function clean($str) {
   return $str;
 }
 ?>
+<link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
 <style>
-body{background:url('subtle_grunge.png')}
-form,p,h1 { text-align: center } 
+body{background:url('subtle_grunge.png');text-align:center}
+h1{font-family: 'Poiret One', cursive;color:#bbb;font-weight:normal;font-size:4em;margin-bottom:0.25em;}
 li {
   background: rgba(255,255,255,0.7);
   list-style: none;
@@ -22,9 +24,32 @@ input{padding: 0.25em}
 form { margin-bottom: 3em}
 form > * { font-size: 130% }
 li img { border: 2px solid rgba(0,0,0,0.25); padding: 0.15em; margin-bottom: 1em }
+#content {
+  background: rgba(255,255,255,0.5);
+  padding: 0.5em;
+  margin-bottom: 1.5em;
+}
+#copy {
+  font-size: 1.2em;
+  font-family: 'Droid Sans', sans-serif;
+  display: inline-block;
+  width: 30em;
+  text-align: left;
+  line-height: 1.5em;
+}
+#joke { border-radius: 12px }
+p { margin: 0.5em}
 </style>
 <h1>Low Contrast Offenders</h1>
-<p>This is where we expose shitty low-contrast impossible to read websites.</p>
+<div id='content'>
+<img id='joke' src='hipster.jpg'>
+<div id="copy">
+<p>Sometimes people have the cool-kid designer stick shoved sooo far up their ass that they forgot to make their content readable.</p>
+<p>Fuck them.</p>
+<p>This is where we expose shitty low-contrast impossible to read websites. </p>
+<p>Screw those poeple. Really.</p>
+</div>
+</div>
 
 <form method='post' action="addoffender.php">
   <input size=40 name="url">
