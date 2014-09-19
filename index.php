032bd5a9698cffb357477582fc9a03047f2cff1a
Chris McKenzie <kristopolous@yahoo.com>
2014-09-18 23:37:33 -0700

    stuff

--- index.php
+++ index.php
@@ -93,7 +93,7 @@ p { margin: 0.5em}
   <div id="copy">
     <p>Some people have the cool-kid designer stick shoved so far up their ass that they forgot to make their content readable.</p>
     <p>This has gone on too long.</p>
-    <p>Let's expose low-contrast impossible to read websites.</p>
+    <p>Let's expose low-contrast impossible to read sites.</p>
     <p>Here's a nickel kid. Get yourself a better website.</p>
   </div>
 </div>
